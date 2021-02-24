476ec9cae95152d0c3840d6cb454e80a4b32fe8e
cyanide126 <cyada2000@gmail.com>
2021-02-24 14:24:23 +0700

    add render

--- routes/add_listing.js
+++ routes/add_listing.js
@@ -12,5 +12,27 @@ exports.view = function(req, res){
   // example:
   // newListing.set("title", req.params.title);
 
+
+myNewObject.set('author', req.query.fauthor);
+myNewObject.set('title', req.query.ftitle);
+myNewObject.set('isbn', req.query.fisbn);
+myNewObject.set('class', req.query.fclass);
+myNewObject.set('price', req.query.fprice);
+myNewObject.set('condition', 'req.query.fcondi');
+myNewObject.set('negotiable', 'No');
+myNewObject.set('image', new Parse.File("resume.txt", { base64: btoa("My file content") }));
+myNewObject.set('notes', 'req.query.fnot');
+
+myNewObject.save().then(
+  (result) => {
+    if (typeof document !== 'undefined') document.write(`Textbook created: ${JSON.stringify(result)}`);
+    console.log('Textbook created', result);
+  },
+  (error) => {
+    if (typeof document !== 'undefined') document.write(`Error while creating Textbook: ${JSON.stringify(error)}`);
+    console.error('Error while creating Textbook: ', error);
+  }
+);
+
   res.render('add_listing', {});
 };
