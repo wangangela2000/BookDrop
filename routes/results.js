e5014353e93b843ffc4f0d798bbc85416f7f64e2
cyanide126 <cyada2000@gmail.com>
2021-02-26 11:42:56 +0700

    images

--- routes/results.js
+++ routes/results.js
@@ -4,38 +4,58 @@
  */
 const Parse = require('parse/node');
 const Textbook = Parse.Object.extend("Textbook");
+
+
 exports.view = function(req, res){
   const query = new Parse.Query(Textbook);
-  const listings = []
 
-  query.fullText('author', req.query.search);
-  query.fullText('title', req.query.search);
-  query.fullText('isbn', req.query.search);
-  query.fullText('class', req.query.search);
-
-  query.find()
-    .then(function(results) {
-      console.log("Successfully retrieved " + results.length + " listings.");
-      for (let i = 0; i < results.length; i++) {
-        const object = results[i];
-        const newListing = {
-          "title" : object.get('title'),
-          "author" : object.get('author'),
-          "price" : object.get('price'),
-          "class" : object.get('class'),
-          "id" : object.id
-        }
-        listings.push(newListing);
-        console.log('Added listing: ' + object.get('id'));
+  function buildListings(results) {
+    const listings = []
+  
+    console.log("Successfully retrieved " + results.length + " listings.");
+    for (let i = 0; i < results.length; i++) {
+      const object = results[i];
+      const newListing = {
+        "title" : object.get('title'),
+        "author" : object.get('author'),
+        "price" : object.get('price'),
+        "class" : object.get('class'),
+        "id" : object.id
       }
-
-      console.log('Number of listings: ' + listings.length);
-        
-      res.render('results', {
-        'listings': listings
-      });
-    })
-    .catch(function(error) {
-      console.log('error! ' + error);
+      listings.push(newListing);
+      console.log('Added listing: ' + object.get('id'));
+    }
+  
+    console.log('Number of listings: ' + listings.length);
+      
+    res.render('results', {
+      'listings': listings
     });
+    
+  
+  }
+
+  var search = req.query.search;
+  console.log(typeof search);
+  if (search == ""){
+    search = " ";
+    query.find()
+      .then(buildListings)
+      .catch(function(error) {
+        console.log('error! ' + error);
+    });
+  } else {
+    query.fullText('author', search);
+    query.fullText('title', search);
+    query.fullText('isbn', search);
+    query.fullText('class', search);
+
+    query.find()
+      .then(buildListings)
+      .catch(function(error) {
+        console.log('error! ' + error);
+    });
+  }
+
+  
 };
